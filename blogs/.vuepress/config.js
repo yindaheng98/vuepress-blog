b25e16212c5c996e9fcf1c2b6a38b4ff2a87e502
yindaheng98 <yindaheng98@163.com>
2021-03-15 21:42:03 +0800

    更新仓库位置

--- blogs/.vuepress/config.js
+++ blogs/.vuepress/config.js
@@ -34,9 +34,9 @@ module.exports = {
   ],
   "theme": require.resolve('./theme'),
   "themeConfig": {
-    "repo": 'yindaheng98/My-docs/tree/master/学习笔记',
+    "repo": 'yindaheng98/Notebook/tree/master/学习笔记',
     "repoLabel": '查看源码',
-    "docsRepo": 'yindaheng98/My-docs',
+    "docsRepo": 'yindaheng98/Notebook',
     "docsDir": '学习笔记',
     "editLinks": true,
     "editLinkText": '帮助我们改善此页面！',
